ef6998b42b2714edb433da73eee943c0f80d5e7a
JatziriHernan12 <jatziryhh44@gmail.com>
2025-11-23 16:46:39 -0600

    Version de las finales

--- app/Models/Respuesta.php
+++ app/Models/Respuesta.php
@@ -9,12 +9,12 @@ class Respuesta extends Model
 {
     use HasFactory;
     protected $table = 'respuesta';
-    protected $fillable = [
+    /*protected $fillable = [
         'id_encuesta',
         'id_pregunta',
         'respuesta_cuanti',
         'respuesta_cuali',
-    ];
+    ];*/
 
     public function encuesta()
     {
